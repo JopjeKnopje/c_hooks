d7923208638238747e33e785c046634b7d257cb2
JopjeKnopje <jboeve009@gmail.com>
2023-11-01 02:06:59 +0100

    trying to overwrite the main function

--- libhook.c
+++ libhook.c
@@ -6,13 +6,17 @@
 #include <string.h>
 
 
-
-ssize_t write(int fildes, const void *buf, size_t nbyte)
+int main()
 {
-	int (*func) (int fildes, const void *buf, size_t nbyte);
-	func = dlsym(RTLD_NEXT, "write");
-
-	char *s =  "overwritten\n\0";
-	func(STDOUT_FILENO, s, strlen(s));
-	return func(fildes, buf, nbyte);
+	printf("haha toch niet\n");
 }
+
+// ssize_t write(int fildes, const void *buf, size_t nbyte)
+// {
+// 	int (*func) (int fildes, const void *buf, size_t nbyte);
+// 	func = dlsym(RTLD_NEXT, "write");
+//
+// 	char *s =  "overwritten\n\0";
+// 	func(STDOUT_FILENO, s, strlen(s));
+// 	return func(fildes, buf, nbyte);
+// }
